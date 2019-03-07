a25542fc22fd7dacf18b1a5d6ef7422aae93313b
Peter Keller <keller.f.peter@gmail.com>
2019-03-07 14:33:25 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
+require "pry"
 def reformat_languages(languages)
-  # your code here
+  result = {}
+  languages.each do |style, language|
+    language.each do |key, value|
+      if !result.key?(key)
+        result[key] = value
+      end
+      result[key][:style] = [style]
+    end
+  end
+  result[:javascript][:style].push(:oo)
+  result
 end
